f2c4c5328a2e239946a63e2c432a6b7fd234cacd
Steven BOEHM <boehm_s@etna-alternance.net>
2018-02-27 18:00:15 +0100

    WIP stop today

--- intercept.js
+++ intercept.js
@@ -1,9 +1,35 @@
-var fs = require('fs');
-var readStream = fs.createReadStream('err.log');
+const spawn = require('child_process').spawn;
+const errStream = spawn('tail', ['-f', './logfiles/err.log']);
+const request = require('request');
 
-var spawn = require('child_process').spawn;
-var tail = spawn('tail', ['-f', 'err.log']);
-
-tail.stdout.on('data', function (data) {
+errStream.stdout.on('data', data => {
     console.log(`DATA : ${data}`);
+});
+
+
+const parseError = error => {
+
+};
+
+const searchSO = ({language, sort, intitle, question}) => new Promise((resolve, reject) => {
+    let uri = `https://api.stackexchange.com/2.2/search/advanced?site=stackoverflow&order=desc`;
+    uri += language ? `&tagged=${language}` : ``;
+    uri += sort     ? `&sort=${sort}`       : `&sort=votes`;
+    uri += intitle  ? `&intitle=${intitle}` : ``;
+    uri += `&q=${encodeURI(question)}`;
+
+    request({uri, gzip: true}, (err, res, body) => err
+	    ? reject(err)
+	    : resolve(JSON.parse(body))
+	   );
+});
+
+
+searchSO({
+    language: 'javascript',
+    intitle: 'eaddrinuse',
+    question: 'Error: EADDRINUSE, Address already in use'
+}).then(json => {
+    console.log(json);
+    console.log(json.length);
 });
